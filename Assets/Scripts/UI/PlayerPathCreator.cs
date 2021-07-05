286aae3a3ca18300533ed9246a7c15b10dee7a1c
Benitk <beny9996@gmail.com>
2021-07-05 17:43:23 +0300

    Update PlayerPathCreator.cs

--- Assets/Scripts/UI/PlayerPathCreator.cs
+++ Assets/Scripts/UI/PlayerPathCreator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1de0a14b02397b963b486e086fa70e5302f552dc8cf436855a98eb64c1c976b6
-size 3034
+oid sha256:6332e0051666a15b229efccac74baece2f73a355be0b171a887c59b35a1034b4
+size 3238
