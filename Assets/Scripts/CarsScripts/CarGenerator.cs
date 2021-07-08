5c566bc0da5dfa4a031625fe73e97cac7fd802a8
HadadOmer <omerhadad10@gmail.com>
2021-07-08 15:23:20 +0300

    Merge branch 'Dev---Omer' into main

--- Assets/Scripts/CarsScripts/CarGenerator.cs
+++ Assets/Scripts/CarsScripts/CarGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1d707a63aab520e1b03d8f0b91c02bfdae6ca9a2c9afc9f6b3e8b4d22542247
-size 1988
+oid sha256:7588cd171dc9fce3e4b2ab00f1af2a65c7ad85169474fa9e6847ca680bbc2d63
+size 2212
--- Assets/Scripts/GameManager/Manager Scripts/CarCounter.cs
+++ Assets/Scripts/GameManager/Manager Scripts/CarCounter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95e7ef34cf460d2557604a86aa741be6e0ba39638c7271eb9ac979d411f45c30
-size 651
+oid sha256:91db4fc27ebe7c35a3516f3a2028f88b903752b72c2e1c753d443249d21b96ea
+size 813
