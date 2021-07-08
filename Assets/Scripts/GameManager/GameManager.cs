7be5aaa71b1c99f1303312c883623eca143aa3cf
AviBoter <maccavi3@gmail.com>
2021-07-08 21:46:49 +0300

    UI

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28e29258320edf83add9e42f485d5705cd44acc9a862cb721bd6748c18c1d965
-size 2714
+oid sha256:9e272aeff0842dcd8bd536c11a1079a3d077ce909c7588f5d7eae52de611082e
+size 2833
