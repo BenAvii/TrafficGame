2eee7563be9a9e53d4118bbe6ac7bae2fe075f15
AviBoter <maccavi3@gmail.com>
2021-07-10 02:23:38 +0300

    Car Crushed

--- Assets/Scripts/CarsScripts/CrashMod.cs
+++ Assets/Scripts/CarsScripts/CrashMod.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b70fcb67c26cd062eae2a4059b35ed9ac4ce7dcc40a7b034dbbb1d35850c2f36
-size 3993
+oid sha256:78fed6a072c277a5acfaff151f7c692586054fc0b23e2f1d5120934ab9458bc3
+size 4100
