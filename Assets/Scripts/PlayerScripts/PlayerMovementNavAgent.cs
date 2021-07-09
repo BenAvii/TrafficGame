851e407342a0ed2c13ca6be4eb688a1e663d81e6
Benitk <beny9996@gmail.com>
2021-07-09 13:50:10 +0300

    Update PlayerMovementNavAgent.cs

--- Assets/Scripts/PlayerScripts/PlayerMovementNavAgent.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovementNavAgent.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a0863ed4244e3979565a230ba0ab54d4facd9c4691f48a57ac32251a29fe447
-size 1064
+oid sha256:5cf6bbd168bd1fb212370c95908eb0854644828cd014200a03a4a12a6e5f10f3
+size 1082
