de43e1a645c37ae1da6d587efe65a35fb9c80ba3
Benitk <beny9996@gmail.com>
2021-07-10 11:24:56 +0300

    add coins collector

---
+++ Assets/DOTween/Modules/DOTweenModulePhysics2D.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:535113318f7db4390a3cfbc66546562a79bafad47fe5cfc1fa23617aa7c7942c
+size 10912
--- Assets/Scripts/CarsScripts/CarInDestination.cs
+++ Assets/Scripts/CarsScripts/CarInDestination.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ab7b8a1a6688e76e9320ae4b53075e0254ae2cefd63765fc3b58a4c33a0bfc9
-size 1811
+oid sha256:4d5ff1a44d9bca78d554693d61de8d56e7268b3c4ecd58c73e982a39d9123495
+size 1831
