1f171e4b5bb52e8e45ffb729a53963da11084a71
AviBoter <maccavi3@gmail.com>
2021-07-12 02:54:36 +0300

    Up

--- Assets/Scripts/GameManager/GameUI Scripts/GameUI.cs
+++ Assets/Scripts/GameManager/GameUI Scripts/GameUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa65aa1b6ea574287d31ef3a3a30e0b3df66b32eeaaed888733291431fa0336f
-size 3371
+oid sha256:1400df3f62da665be225e69970d2015094e2d457438275f2889be0a7187d58e3
+size 3379
