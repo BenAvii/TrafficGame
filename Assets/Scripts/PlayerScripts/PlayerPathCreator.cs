8f0ae70e31c540d653c2dd95b9926affda7f1e73
AviBoter <maccavi3@gmail.com>
2021-07-11 23:41:14 +0300

    UP

--- Assets/Scripts/PlayerScripts/PlayerPathCreator.cs
+++ Assets/Scripts/PlayerScripts/PlayerPathCreator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08aca0336ebe4a13e555b1b1132fddd97b0e93abe66ba87f4b9db0de909727ed
-size 5668
+oid sha256:ae6776d7e353b042f9d862c1b6c706fe4f6c2cc9b8c922020292169157b07c8c
+size 5672
