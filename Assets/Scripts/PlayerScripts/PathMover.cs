ae9492b5100b32ec135bd1932974c85e3700a040
AviBoter <maccavi3@gmail.com>
2021-07-11 18:44:58 +0300

    Bugs fixed

--- Assets/Scripts/PlayerScripts/PathMover.cs
+++ Assets/Scripts/PlayerScripts/PathMover.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7c7df668d5101790a4295f59dbcc3a53fe4600e02b0eeeec14580b270cad31f
-size 4771
+oid sha256:7253ffa1995fcfb36f77e0f9712cc35f204bfe107871d91ea4592e21f7426ae1
+size 4793
--- Assets/Scripts/PlayerScripts/PlayerPathCreator.cs
+++ Assets/Scripts/PlayerScripts/PlayerPathCreator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b4445b5a1b92871a6dfa04a6cf20de1d38e6e7250c4765e37364a44d1dd3894
-size 5212
+oid sha256:a97841da996f60f360edfce74ef9539e569f100ec9eac18f3211e904da877c6b
+size 5615
