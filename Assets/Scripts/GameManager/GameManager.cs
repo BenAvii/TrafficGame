7f06fe00a86020790d2ab1c177008fab2aad52c1
HadadOmer <omerhadad10@gmail.com>
2021-07-12 10:31:20 +0300

    Tutorial Fixes

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbaedc322ef88f6944c0a24eebca00df0eed9af10d7b861a68d250655f6882f4
-size 4566
+oid sha256:06722b51b3a6b381c731a5098643638ffa451b308000049e2b931e3195ca61ec
+size 4556
