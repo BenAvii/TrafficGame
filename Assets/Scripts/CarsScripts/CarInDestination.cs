ea91f2fdb35dc3357ddabf99cf262d708bf14bf7
HadadOmer <omerhadad10@gmail.com>
2021-07-13 19:10:41 +0300

    Mini map And tutorial

--- Assets/Scripts/CarsScripts/CarInDestination.cs
+++ Assets/Scripts/CarsScripts/CarInDestination.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfc5af5a09679bb19f616a9fec936f52ea4edb2d4ad74fd23967890ed16b6093
-size 2494
+oid sha256:4cc210ff52467e790e9bc85df9385cff0b96e0e26cf62107ec1bfbd323665911
+size 2483
