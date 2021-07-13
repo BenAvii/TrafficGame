2ca92228965c5dd7f9318b9e358729c469adf7bc
Benitk <beny9996@gmail.com>
2021-07-13 11:30:12 +0300

    commit

--- Assets/Scripts/CarsScripts/CarGenerator.cs
+++ Assets/Scripts/CarsScripts/CarGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0a27b6383de43c782f764166c7c171d81a6a9bb156c4e7d621984238ef122cb
-size 2509
+oid sha256:c21f781fc0a796b1e4e26a8155f2bfef6370476cc67df2d2407ca7bea4546a43
+size 2499
--- Assets/Scripts/PlayerScripts/PathMover.cs
+++ Assets/Scripts/PlayerScripts/PathMover.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fff5b466449ddce5f88f8b7d0afd130ca0602211760be239483af79456749b90
-size 4771
+oid sha256:66027ec4fb80b40bf86550905714914c101b352b47dffb91fbd987f2cfd7b771
+size 4901
