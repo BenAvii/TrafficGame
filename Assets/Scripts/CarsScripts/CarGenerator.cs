1192f11faf0b516d9ad9c6c79289b81555b1737d
HadadOmer <omerhadad10@gmail.com>
2021-07-13 17:00:51 +0300

    Fmod Plugins and audio scripts

--- Assets/Scripts/CarsScripts/CarGenerator.cs
+++ Assets/Scripts/CarsScripts/CarGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c90c7ead888b82644f54a63bcaa67f1d9645f8351c5366e0e7df4eaf93247743
-size 3012
+oid sha256:fb82f92cf2c8ac5bdd663f607ac70f1806ca8272912606edb3771d8f8c53c461
+size 3098
---
+++ Assets/Scripts/CarsScripts/EngineSound.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8bfc2e635ff5619fcb49fbbcad5a9f0f0bdda5c4dd0e66c4b05b6841997a5fed
+size 1221
