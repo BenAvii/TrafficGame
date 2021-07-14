59dfabd06189c983e340b33d111277d1efb4d04c
HadadOmer <omerhadad10@gmail.com>
2021-07-14 11:51:34 +0300

    MiniMap Fixes

--- Assets/Scripts/UI/MiniMapHandler.cs
+++ Assets/Scripts/UI/MiniMapHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c339c5148db8fee31e904edcf84bab64eabdc95b7d48ffe94f134f865d40c4a8
-size 2213
+oid sha256:a7140342ddf17446c913d0096d6606718eb22e0575855dfd57542cb045d13e24
+size 2619
